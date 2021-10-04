fc4b3ea5729ab462fde2c70f74e6d15b8ebed0c5
Ifiok Jr <ifiokotung@gmail.com>
2021-10-04 20:02:59 +0100

    chore: update config files

--- .monots/enable-cli.js
+++ .monots/enable-cli.js
@@ -22,6 +22,7 @@ const files = [
   baseDir('packages/monots__types/package.json'),
   baseDir('packages/monots__cli/package.json'),
   baseDir('packages/superstruct-extra/package.json'),
+  baseDir('packages/superstruct-converter/package.json'),
 ];
 
 async function run() {
