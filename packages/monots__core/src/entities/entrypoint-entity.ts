c31447902e1787efbb7ed272b5f398811b5b98c8
Ifiok Jr <ifiokotung@gmail.com>
2021-11-01 13:24:46 +0000

    fix: broken build

--- packages/monots__core/src/entities/entrypoint-entity.ts
+++ packages/monots__core/src/entities/entrypoint-entity.ts
@@ -6,13 +6,7 @@ import path from 'node:path';
 import normalizePath from 'normalize-path';
 
 import { createTypeScriptContent, entries, generateField } from '../helpers/index.js';
-import {
-  Entrypoint,
-  Entrypoint,
-  EntrypointField,
-  entrypointFields,
-  ExportsField,
-} from '../schema.js';
+import { Entrypoint, EntrypointField, entrypointFields, ExportsField } from '../schema.js';
 import { BaseEntity, BaseEntityProps } from './base-entity.js';
 import type { PackageEntity } from './package-entity.js';
 
